2cba14ff7d0614b4eeb45b5d02228e6d925139ae
VinuthaSadashiv <83771224+VinuthaSadashiv@users.noreply.github.com>
2021-07-09 03:24:45 +0530

    Update student_data_analysis.py

--- student_data_analysis.py
+++ student_data_analysis.py
@@ -12,7 +12,7 @@ def Mathematics(student_list):
             highest_score_maths = every_student.get('Maths')
             student_name = every_student.get('Name')
 
-    print(f'Highest score in Mathematics is {highest_score_maths} by {student_name}')
+    print(f'Highest score in Mathematics is {highest_score_maths} by {student_name}.')
 
 def Science(student_list):
     highest_score_science = 0
@@ -22,7 +22,7 @@ def Science(student_list):
             highest_score_science = every_student.get('Science')
             student_name = every_student.get('Name')
 
-    print(f'Highest score in Science is {highest_score_science} by {student_name}')
+    print(f'Highest score in Science is {highest_score_science} by {student_name}.')
 
 def Ethics(student_list):
     highest_score_ethics = 0
@@ -32,7 +32,7 @@ def Ethics(student_list):
             highest_score_ethics = every_student.get('Ethics')
             student_name = every_student.get('Name')
 
-    print(f'Highest score in Ethics is {highest_score_ethics} by {student_name}')
+    print(f'Highest score in Ethics is {highest_score_ethics} by {student_name}.')
 
 Mathematics(student_list)
 Science(student_list)
